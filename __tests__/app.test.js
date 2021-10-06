7b2e50017b51a4290ee5afebf7f2d564023fd4f0
satyadvaya <satyadvaya@gmail.com>
2021-10-05 19:26:18 -0700

    failing test GET all species

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,7 +8,7 @@ describe('species routes', () => {
     return setup(pool);
   });
 
-  it('should add a new species', () => {
+  it('should POST a new species', () => {
     const newSpecies = {
       speciesType: 'Lorax',
       extinct: true,
@@ -17,8 +17,21 @@ describe('species routes', () => {
       .post('/api/species')
       .send(newSpecies)
       .then((res) => {
-        console.log(res.body);
         expect(res.body).toEqual({ ...newSpecies, id: '6' });
+      });
+  });
+
+  it('should GET all species', () => {
+    return request(app)
+      .get('/api/species')
+      .then((res) => {
+        expect(res.body).toEqual([
+          { id: expect.any(Number), speciesType: 'Fish', extinct: false },
+          { id: expect.any(Number), speciesType: 'Amphibian', extinct: false },
+          { id: expect.any(Number), speciesType: 'Reptile', extinct: false },
+          { id: expect.any(Number), speciesType: 'Bird', extinct: false },
+          { id: expect.any(Number), speciesType: 'Thylacine', extinct: true },
+        ]);
       });
   });
 
